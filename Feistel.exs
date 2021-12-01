fcc955f9436e78c701b49b24799f833b0386d54b
miguel7799 <42215165+miguel7799@users.noreply.github.com>
2021-12-01 13:03:37 -0600

    Pirmeas finciones

---
+++ Feistel.exs
@@ -0,0 +1,46 @@
+#Final-Project
+#
+#Miguel Hernández A01022398
+#Lorenzo Jacome A0102
+#
+#Feistel Algorithm
+
+
+defmodule Feistel do
+
+  def reverse(list), do: do_reverse(list, [])
+    defp do_reverse([], listreturn),
+       do: listreturn
+    defp do_reverse([head | tail], listreturn) do
+      if is_list(head) do
+        do_reverse(tail, [reverse(head) | listreturn])
+      else
+        do_reverse(tail, [head | listreturn])
+      end
+    end
+
+    def split(list) do
+      len = round(length(list)/2)
+      Enum.split(list, len)
+    end
+
+
+    def concat(list1, list2) do
+      list1 ++ list2
+    end
+
+    def decimal_to_binary(str) when is_binary(str) do
+      str
+      |> String.to_integer()
+      |> decimal_to_binary()
+    end
+
+    def decimal_to_binary(num) when is_integer(num) do
+      binary_string = :erlang.integer_to_binary(num, 2)
+      binary_string_length = String.length(binary_string)
+      desired_string_length = binary_string_length + (8 - rem(binary_string_length, 8))
+      String.pad_leading(binary_string, desired_string_length, "0")
+    end
+
+
+  end
